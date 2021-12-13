1f2e5fa05a8fb4b872f90210a287d263b1ba5baf
ACER <ACER@LAPTOP-MOGB7KD1>
2021-12-13 09:11:45 +0700

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bilangan_prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bilangan;
+            Console.Write("Masukkan batas bilangan : ");
+            bilangan = Convert.ToInt32(Console.ReadLine());
+            int temp = 0;
+            Console.WriteLine();
+          
